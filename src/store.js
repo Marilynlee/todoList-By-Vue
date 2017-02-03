6639b4e6dba2131fadc02990df2f14a504c97ba4
wb-lyn227583 <wb-lyn227583@alibaba-inc.com>
2017-02-03 16:49:13 +0800

    add files

---
+++ src/store.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lyn on 2017/2/3.
+ */
+const STORAGE_KEY="todos-vuejs";
+export default {
+    fetch(){
+        return JSON.parse(window.localStorage.getItem(STORAGE_KEY||"[]"))
+    },
+    save(items){
+        window.localStorage.setItem(STORAGE_KEY,JSON.stringify(items))
+    }
+}
