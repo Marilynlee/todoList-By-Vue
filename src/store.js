9176aa01899e7fcbb71daa6d9714a3889c33a0d0
Marilyn <Marilynlee@users.noreply.github.com>
2017-02-04 10:52:14 +0800

    fixed store.js file error
    modified app.vue and store.js files.

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@
 const STORAGE_KEY="todos-vuejs";
 export default {
     fetch(){
-        return JSON.parse(window.localStorage.getItem(STORAGE_KEY||"[]"))
+        return JSON.parse(window.localStorage.getItem(STORAGE_KEY))||[];
     },
     save(items){
         window.localStorage.setItem(STORAGE_KEY,JSON.stringify(items))
